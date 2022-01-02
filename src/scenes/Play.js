f0c12abd07c5f586921373d736be9c2f7670eb36
risrobs <robertivanov22@abv.bg>
2022-01-02 21:16:55 +0200

    12

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,12 +1,31 @@
-import Footer from "../components/Footer";
-import Scene from "./Scene";
+import Scene from './Scene';
+import gsap from 'gsap';
+import Footer from '../components/Footer';
+import Button from '../components/Button';
+import Pokeball from '../components/Pokeball';
 
 export default class Play extends Scene {
   async onCreated() {
+
     const footer = new Footer();
-    footer.x = -window.innerWidth / 2;
+    footer.x = - window.innerWidth / 2;
     footer.y = window.innerHeight / 2 - footer.height;
     this.addChild(footer);
+
+    const pokeball = new Pokeball();
+    pokeball.y = -50
+    this.addChild(pokeball)
+
+    const button = new Button();
+    this.addChild(button);
+
+    button.on('click',async () => await pokeball.open())
+    button.on('click',async () => await pokeball._shuffle())
+
+    pokeball.on(Pokeball.events.OPEN_START, () => button.hide())
+    pokeball.on(Pokeball.events.CLOSE_END, () => button.show())
+    pokeball.on(Pokeball.events.SHUFFLE_END, async () => await pokeball.close())
+
   }
 
   /**
@@ -16,7 +35,7 @@ export default class Play extends Scene {
    * @param  {Number} width  Window width
    * @param  {Number} height Window height
    */
-  onResize(width, height) {
-    // eslint-disable-line no-unused-vars
+  onResize(width, height) { // eslint-disable-line no-unused-vars
+
   }
 }
